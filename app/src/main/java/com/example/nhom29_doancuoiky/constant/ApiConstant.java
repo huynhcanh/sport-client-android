bde29c50c6652d5a16563563fc35c77e95bccfa7
Hoang Johnny <johnnyhoang482@gmail.com>
2022-05-19 08:48:57 +0700

    finalx

--- app/src/main/java/com/example/nhom29_doancuoiky/constant/ApiConstant.java
+++ app/src/main/java/com/example/nhom29_doancuoiky/constant/ApiConstant.java
@@ -3,7 +3,7 @@ package com.example.nhom29_doancuoiky.constant;
 import com.example.nhom29_doancuoiky.response.UserApiResponse;
 
 public class ApiConstant {
-    public final static String URL_API = "https://1785-115-77-72-165.ngrok.io/api/";
-    public final static String URL_IMG = "https://1785-115-77-72-165.ngrok.io/image/";
+    public final static String URL_API = "https://17e3-180-93-146-1.ngrok.io/api/";
+    public final static String URL_IMG = "https://17e3-180-93-146-1.ngrok.io/image/";
     public static UserApiResponse userLog = null;
 }
